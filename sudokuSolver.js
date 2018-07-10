d3cd8d54436062e8e2ca00bcd5a7e5fc2c4bc706
Raam Chauhan <raam.chauhan@softwire.com>
2018-07-10 15:00:29 +0100

    Add function to generate children.

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,40 +1,25 @@
 const Grid = require('./grid');
 const fs = require('fs');
 
-function readInputFile(filepath) {
-    return fs.readFileSync(filepath).toString('utf8').split(' \n').map(r => r.split(' ').map(c => parseInt(c)));
-}
-
 // input grid is considered as grid 0
-const input = readInputFile('./hard.txt');
+const input = readInputFile('./medium.txt');
 
 // initial grid is grid 1, with the input grid as a parent
-let gridCounter = 1;
-const initialGrid = new Grid(input, gridCounter, 0);
-const initialCompleteness = initialGrid.calculateCompleteness();
-console.log(initialCompleteness);
+let gridIdCounter = 1;
+const initialGrid = new Grid(input, gridIdCounter, 0);
+console.log(`Initial score: ${initialGrid.calculateCompleteness()}%`);
 
 // list of all grids
 const grids = [];
 grids.push(initialGrid);
 
+let currentGridId = 1;
+
 initialGrid.updateGridWithoutBranching();
-console.log(initialGrid.calculateCompleteness())
+console.log(initialCompleteness);
 
-
-
-// const nextCell = initialGrid.findNextUnsolvedCell();
-// const options = initialGrid.getOptionsForCell(nextCell.row, nextCell.column);
-// console.log(options);
-
-// for (let i = 0; i < options.length; i++) {
-//     const childGrid = new Grid(initialGrid.gridArray, gridCounter, 1)
-//     childGrid.updateCell(nextCell.row, nextCell.column,options[i]);
-//     console.log(childGrid.isValid())
-//     grids.push(childGrid);
-//     gridCounter++;
-
-// }
+generateValidChildren(initialGrid)
+console.log(grids)
 
 
 
@@ -42,3 +27,25 @@ console.log(initialGrid.calculateCompleteness())
 // to hold a snapshot in time
 // it potentially has a parent and children. This helps to move up and down branches in the tree
 // it has methods on it to determine if it is valid and complete
+
+// helper methods
+
+function generateValidChildren(parentGrid) {
+    const nextCell = parentGrid.nextUnsolvedCell;
+    const options = parentGrid.getOptionsForCell(nextCell.row, nextCell.column);
+    
+    for (let i = 0; i < options.length; i++) {
+        const childGrid = new Grid(parentGrid.gridArray, gridIdCounter, parentGrid.gridId)
+        childGrid.updateCell(nextCell.row, nextCell.column, options[i]);
+        if (childGrid.isValid()) {
+            grids.push(childGrid);
+            gridIdCounter++;
+        }
+    }
+}
+
+// grids.filter(g => g.gridId === 1) 
+
+function readInputFile(filepath) {
+    return fs.readFileSync(filepath).toString('utf8').split(' \n').map(r => r.split(' ').map(c => parseInt(c)));
+}
