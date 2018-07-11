e69d0b9f67263d61fe7d15850644f136186a8f6d
Raam Chauhan <raam.chauhan@softwire.com>
2018-07-11 15:36:35 +0100

    Rewrite while loop logic.

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -16,44 +16,89 @@ if (isOptimized) {
 initialGrid.findNextUnsolvedCell();
 
 // list of all grids
-const grids = [];
+let grids = [];
 grids.push([initialGrid]);
 
-let latestGeneration = grids[grids.length - 1];
-while (latestGeneration.length != 1 || !latestGeneration[0].isComplete()) {
-    
-    if (latestGeneration.length == 1) {
-        grids.push(generateValidChildren(latestGeneration[0]));
+// let latestGeneration = grids[grids.length - 1];
+// while (latestGeneration.length != 1 || !latestGeneration[0].isComplete()) {
 
-        // and has valid children then create them and carry on
-        // if no valid children, delete it and it's parent
-    } else {
-        const parentGrid = latestGeneration[0];
-        const children = generateValidChildren(parentGrid)
+//     const parentGrid = latestGeneration[0];
+//     const children = generateValidChildren(parentGrid);
+//     console.log(children)
 
-        if (children.length > 0) {
-            grids.push(children)
+//     if (children.length > 0) {
+//         grids.push(children);
+//     } else {
+//         if (latestGeneration.length == 1) {
+//             grids.pop();
+//         } else {
+//             grids[grids.length - 1] = latestGeneration.filter(g => g.gridId != parentGrid.gridId);
+//         }
+//     }
+
+// // if(parentGrid.gridId==9){
+// //     console.log(parentGrid.gridArray)
+// // }
+
+//     latestGeneration = grids[grids.length - 1];
+//     // console.log(latestGeneration[0].calculateCompleteness())
+//     // console.log(latestGeneration[0].gridArray)
+
+//     console.log(`Number of generations: ${grids.length}`)
+//     console.log(`Number of grids in each generation: ${grids.map(gen => gen.length)}`)
+//     console.log(`Grid IDs in each generation: ${grids.map(gen => gen.map(grid => grid.gridId))}`)
+
+//     console.log(' ')
+
+
+console.log(grids[grids.length-1][0])
+
+
+while (!grids[grids.length - 1][0].isComplete()) {
+    // while 1st child of final generation is not complete
+
+    if (grids[grids.length - 1].length == 1) {
+        // if there is only 1 child in the generation
+        
+        const children = generateValidChildren(grids[grids.length - 1][0]);
+        if (children.length === 0) {
+            // if that child has no valid children of it's own, delete the generation
+            // and delete it's parent
+            console.log('a')
+            grids.pop()
+            grids[grids.length - 1] = grids[grids.length - 1].slice(1);
         } else {
-            grids[grids.length - 1] = latestGeneration.filter(g => g.gridId != parentGrid.gridId);
-
-            // remove a grid from generation if you have tried but were unable to create successful children
-            // if this is the lasst grid of that generation and it doesn't have valid children, 
-            // then you also need to removve it's parent
+            // if there are valid children, create a new generation
+            console.log('b')
+            
+            grids.push(children);
         }
-        latestGeneration = grids[grids.length - 1];
     }
+    
+    else {
+        // if there are multiple children in a generation
+        // take the first child
+        const children = generateValidChildren(grids[grids.length-1][0])
+        if(children.length >0) {
+            console.log('c')
+            // if that child has valid children, create a new generation
+            
+            grids.push(children);
+        } else {
+            console.log('d')
+            // otherwise delete it from the generation
+            grids[grids.length - 1] = grids[grids.length - 1].slice(1);
+        }
+
+    }
+
+    console.log(`Number of generations: ${grids.length}`)
+    console.log(`Number of grids in each generation: ${grids.map(gen => gen.length)}`)
+    console.log(`Grid IDs in each generation: ${grids.map(gen => gen.map(grid => grid.gridId))}`)
+    console.log(' ')
+    // console.log(grids[grids.length - 1][0])
+    
 }
-
-console.log(initialGrid.gridArray);
-console.log(latestGeneration[0].gridArray);
-console.log('Grid complete');
-
-
-
-// console.log(grids)
-
-
-
 
 
 
@@ -64,7 +109,7 @@ console.log('Grid complete');
 
 function generateValidChildren(parentGrid) {
     const nextCell = parentGrid.findNextUnsolvedCell();
-    const options = parentGrid.getOptionsForCell(nextCell.row, nextCell.column);
+    const options = parentGrid.getOptionsForCell(nextCell.row, nextCell.column)
     const children = [];
     for (let i = 0; i < options.length; i++) {
         const childGrid = new Grid(parentGrid.gridArray, gridIdCounter, parentGrid.gridId)
